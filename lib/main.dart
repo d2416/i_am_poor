715a7429d808699efad6f606ba61ea86258a7203
David <davidpineda.co@gmail.com>
2021-01-21 17:13:03 +0000

    Add image

--- lib/main.dart
+++ lib/main.dart
@@ -8,9 +8,9 @@ class IamPoorApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.brown[300],
+        backgroundColor: Colors.brown[200],
         appBar: AppBar(
-          backgroundColor: Colors.grey,
+          backgroundColor: Colors.teal[500],
           title: Text("I am Poor"),
         ),
         body: Center(
